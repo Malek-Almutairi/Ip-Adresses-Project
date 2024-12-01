4d386ff2dc41adb97ea550a1e1b590c4789ea289
Malek-Almutairi <malekalmutairi12@gmail.com>
2024-12-01 22:41:01 +0300

    Initial commit

---
+++ IPAddress/Form1.Designer.cs
@@ -0,0 +1,388 @@
+﻿namespace IPAddress
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txt1 = new System.Windows.Forms.TextBox();
+            this.txt2 = new System.Windows.Forms.TextBox();
+            this.txt3 = new System.Windows.Forms.TextBox();
+            this.txt4 = new System.Windows.Forms.TextBox();
+            this.BtnCheck = new System.Windows.Forms.Button();
+            this.BtnDetect = new System.Windows.Forms.Button();
+            this.BtnCalc = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.lblMsg = new System.Windows.Forms.Label();
+            this.m4 = new System.Windows.Forms.TextBox();
+            this.m3 = new System.Windows.Forms.TextBox();
+            this.m2 = new System.Windows.Forms.TextBox();
+            this.m1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(127, 126);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(55, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "IPAddress";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // txt1
+            // 
+            this.txt1.Location = new System.Drawing.Point(182, 123);
+            this.txt1.MaxLength = 3;
+            this.txt1.Name = "txt1";
+            this.txt1.Size = new System.Drawing.Size(36, 20);
+            this.txt1.TabIndex = 2;
+            this.txt1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // txt2
+            // 
+            this.txt2.Location = new System.Drawing.Point(223, 123);
+            this.txt2.MaxLength = 3;
+            this.txt2.Name = "txt2";
+            this.txt2.Size = new System.Drawing.Size(36, 20);
+            this.txt2.TabIndex = 3;
+            this.txt2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // txt3
+            // 
+            this.txt3.Location = new System.Drawing.Point(266, 123);
+            this.txt3.MaxLength = 3;
+            this.txt3.Name = "txt3";
+            this.txt3.Size = new System.Drawing.Size(36, 20);
+            this.txt3.TabIndex = 4;
+            this.txt3.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // txt4
+            // 
+            this.txt4.Location = new System.Drawing.Point(307, 123);
+            this.txt4.MaxLength = 3;
+            this.txt4.Name = "txt4";
+            this.txt4.Size = new System.Drawing.Size(36, 20);
+            this.txt4.TabIndex = 5;
+            this.txt4.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // BtnCheck
+            // 
+            this.BtnCheck.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(192)))));
+            this.BtnCheck.ForeColor = System.Drawing.Color.White;
+            this.BtnCheck.Location = new System.Drawing.Point(-1, 53);
+            this.BtnCheck.Name = "BtnCheck";
+            this.BtnCheck.Size = new System.Drawing.Size(110, 40);
+            this.BtnCheck.TabIndex = 6;
+            this.BtnCheck.Text = "Check Validity";
+            this.BtnCheck.UseVisualStyleBackColor = false;
+            this.BtnCheck.Click += new System.EventHandler(this.BtnCheck_Click);
+            // 
+            // BtnDetect
+            // 
+            this.BtnDetect.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(192)))));
+            this.BtnDetect.ForeColor = System.Drawing.Color.White;
+            this.BtnDetect.Location = new System.Drawing.Point(-1, 93);
+            this.BtnDetect.Name = "BtnDetect";
+            this.BtnDetect.Size = new System.Drawing.Size(110, 40);
+            this.BtnDetect.TabIndex = 7;
+            this.BtnDetect.Text = "Detect Class";
+            this.BtnDetect.UseVisualStyleBackColor = false;
+            this.BtnDetect.Click += new System.EventHandler(this.BtnDetect_Click);
+            // 
+            // BtnCalc
+            // 
+            this.BtnCalc.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(192)))));
+            this.BtnCalc.ForeColor = System.Drawing.Color.White;
+            this.BtnCalc.Location = new System.Drawing.Point(-1, 133);
+            this.BtnCalc.Name = "BtnCalc";
+            this.BtnCalc.Size = new System.Drawing.Size(110, 40);
+            this.BtnCalc.TabIndex = 8;
+            this.BtnCalc.Text = "Calculate Musk";
+            this.BtnCalc.UseVisualStyleBackColor = false;
+            this.BtnCalc.Click += new System.EventHandler(this.BtnCalc_Click);
+            // 
+            // button5
+            // 
+            this.button5.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(192)))));
+            this.button5.ForeColor = System.Drawing.Color.White;
+            this.button5.Location = new System.Drawing.Point(-1, 246);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(110, 40);
+            this.button5.TabIndex = 9;
+            this.button5.Text = "Binary Address";
+            this.button5.UseVisualStyleBackColor = false;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(192)))));
+            this.button6.ForeColor = System.Drawing.Color.White;
+            this.button6.Location = new System.Drawing.Point(-1, 210);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(110, 40);
+            this.button6.TabIndex = 10;
+            this.button6.Text = "Broadcast Address";
+            this.button6.UseVisualStyleBackColor = false;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button7
+            // 
+            this.button7.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(192)))));
+            this.button7.ForeColor = System.Drawing.Color.White;
+            this.button7.Location = new System.Drawing.Point(-1, 171);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(110, 40);
+            this.button7.TabIndex = 11;
+            this.button7.Text = "Network address";
+            this.button7.UseVisualStyleBackColor = false;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // lblMsg
+            // 
+            this.lblMsg.AutoSize = true;
+            this.lblMsg.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblMsg.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
+            this.lblMsg.Location = new System.Drawing.Point(126, 214);
+            this.lblMsg.Name = "lblMsg";
+            this.lblMsg.Size = new System.Drawing.Size(0, 24);
+            this.lblMsg.TabIndex = 12;
+            // 
+            // m4
+            // 
+            this.m4.Location = new System.Drawing.Point(307, 149);
+            this.m4.MaxLength = 3;
+            this.m4.Name = "m4";
+            this.m4.Size = new System.Drawing.Size(36, 20);
+            this.m4.TabIndex = 18;
+            this.m4.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // m3
+            // 
+            this.m3.Location = new System.Drawing.Point(266, 149);
+            this.m3.MaxLength = 3;
+            this.m3.Name = "m3";
+            this.m3.Size = new System.Drawing.Size(36, 20);
+            this.m3.TabIndex = 17;
+            this.m3.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // m2
+            // 
+            this.m2.Location = new System.Drawing.Point(223, 149);
+            this.m2.MaxLength = 3;
+            this.m2.Name = "m2";
+            this.m2.Size = new System.Drawing.Size(36, 20);
+            this.m2.TabIndex = 16;
+            this.m2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // m1
+            // 
+            this.m1.Location = new System.Drawing.Point(182, 149);
+            this.m1.MaxLength = 3;
+            this.m1.Name = "m1";
+            this.m1.Size = new System.Drawing.Size(36, 20);
+            this.m1.TabIndex = 15;
+            this.m1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(131, 152);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(33, 13);
+            this.label2.TabIndex = 14;
+            this.label2.Text = "Mask";
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.Silver;
+            this.panel1.Location = new System.Drawing.Point(-1, 291);
+            this.panel1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(419, 18);
+            this.panel1.TabIndex = 19;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.White;
+            this.label3.Location = new System.Drawing.Point(77, 0);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(272, 26);
+            this.label3.TabIndex = 20;
+            this.label3.Text = "IP Address Identification";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.Color.White;
+            this.label4.Location = new System.Drawing.Point(72, 23);
+            this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(281, 26);
+            this.label4.TabIndex = 21;
+            this.label4.Text = "And Calculation Software";
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(192)))));
+            this.panel2.Controls.Add(this.label3);
+            this.panel2.Controls.Add(this.label4);
+            this.panel2.Location = new System.Drawing.Point(-1, 2);
+            this.panel2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(419, 47);
+            this.panel2.TabIndex = 22;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
+            this.label5.Location = new System.Drawing.Point(133, 260);
+            this.label5.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(0, 13);
+            this.label5.TabIndex = 23;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
+            this.label6.Location = new System.Drawing.Point(200, 260);
+            this.label6.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(0, 13);
+            this.label6.TabIndex = 24;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
+            this.label7.Location = new System.Drawing.Point(254, 260);
+            this.label7.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(0, 13);
+            this.label7.TabIndex = 25;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
+            this.label8.Location = new System.Drawing.Point(319, 260);
+            this.label8.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(0, 13);
+            this.label8.TabIndex = 26;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(444, 336);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.m4);
+            this.Controls.Add(this.m3);
+            this.Controls.Add(this.m2);
+            this.Controls.Add(this.m1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.lblMsg);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.BtnCalc);
+            this.Controls.Add(this.BtnDetect);
+            this.Controls.Add(this.BtnCheck);
+            this.Controls.Add(this.txt4);
+            this.Controls.Add(this.txt3);
+            this.Controls.Add(this.txt2);
+            this.Controls.Add(this.txt1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txt1;
+        private System.Windows.Forms.TextBox txt2;
+        private System.Windows.Forms.TextBox txt3;
+        private System.Windows.Forms.TextBox txt4;
+        private System.Windows.Forms.Button BtnCheck;
+        private System.Windows.Forms.Button BtnDetect;
+        private System.Windows.Forms.Button BtnCalc;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Label lblMsg;
+        private System.Windows.Forms.TextBox m4;
+        private System.Windows.Forms.TextBox m3;
+        private System.Windows.Forms.TextBox m2;
+        private System.Windows.Forms.TextBox m1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+    }
+}
+
---
+++ IPAddress/Form1.cs
@@ -0,0 +1,308 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace IPAddress
+{
+    public partial class Form1 : Form
+    {
+        int flag2 = -1;
+        int flag1 = -1;
+        string convert_to_Binary(int n)
+        {
+            int i;
+            int[] a = new int[8];
+            for (i = 0; n > 0; i++)
+            {
+                a[i] = n % 2;
+                n = n / 2;
+            }
+
+            string x = "";
+            for (i = 7; i >= 0; i--)
+            {
+                x += a[i];
+            }
+            return x;
+
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void BtnDetect_Click(object sender, EventArgs e)
+        {
+
+            if (int.Parse(txt1.Text) >= 0 && int.Parse(txt1.Text) <= 127)
+            {
+                lblMsg.Text = "Class A";
+                flag2 = 1;
+            }
+            else if (int.Parse(txt1.Text) >= 128 && int.Parse(txt1.Text) <= 191)
+            {
+                lblMsg.Text = "Class B";
+                flag2 = 2;
+
+            }
+               
+            else if (int.Parse(txt1.Text) >= 192 && int.Parse(txt1.Text) <= 223)
+            {
+                lblMsg.Text = "Class C";
+                flag2 = 3;
+
+            }
+               
+            else if (int.Parse(txt1.Text) >= 224 && int.Parse(txt1.Text) <= 239)
+            {
+                lblMsg.Text = "Class D";
+                flag2 = 4;
+
+            }
+                
+            else if (int.Parse(txt1.Text) >= 240 && int.Parse(txt1.Text) <= 255)
+            {
+                lblMsg.Text = "class E";
+                flag2 = 4;
+
+            }
+                
+        }
+
+      
+        private void BtnCheck_Click(object sender, EventArgs e)
+        {
+            lblMsg.Text = "";
+            int flag5 = -1;
+            int flag6 = -1;
+            int flag7 = -1;
+            Boolean t1 = false, t2 = false, t3 = false, t4 = false;
+
+            if (int.Parse(txt1.Text) >= 0 && int.Parse(txt1.Text) <= 255)
+            {
+                t1 = true;
+                
+                
+
+
+            }
+
+            if (int.Parse(txt2.Text) >= 0 && int.Parse(txt2.Text) <= 255)
+
+            {
+                t2 = true;
+                if (int.Parse(txt2.Text) == 0)
+                {
+                    flag5 = 1;
+                }
+            }
+            if (int.Parse(txt3.Text) >= 0 && int.Parse(txt3.Text) <= 255)
+
+            {
+                t3 = true;
+                if (int.Parse(txt3.Text) == 0)
+                {
+                    flag6 = 1;
+                }
+            }
+            if (int.Parse(txt4.Text) >= 0 && int.Parse(txt4.Text) <= 255)
+            {
+                t4 = true;
+                flag1 = 1;
+                if (int.Parse(txt4.Text) == 0)
+                {
+                    flag7 = 1;
+                }
+            }
+            if (int.Parse(txt1.Text) <= 127)
+            {
+                if (flag5 == -1 || flag6 == -1 || flag7 == -1)
+                { 
+                    if (t1 == true && t2 == true && t3 == true && t4 == true)
+                    { 
+                            
+                        lblMsg.Text = "IP Address is Valid";
+                        lblMsg.ForeColor = Color.Green;
+                    }
+                    else
+                    {
+                        lblMsg.Text = "IP Address is not Valid";
+                        lblMsg.ForeColor = Color.Red;
+                    }
+
+                }
+                else
+                {
+                    lblMsg.Text = "IP Address is not Valid";
+                    lblMsg.ForeColor = Color.Red;
+                }
+            }
+
+            if (int.Parse(txt1.Text) >= 128 && int.Parse(txt1.Text) <= 191)
+            {
+                if (flag6 == -1 || flag7 == -1)
+                {
+                    if (t1 == true && t2 == true && t3 == true && t4 == true)
+                    {
+
+                        lblMsg.Text = "IP Address is Valid";
+                        lblMsg.ForeColor = Color.Green;
+                    }
+                    else
+                    {
+                        lblMsg.Text = "IP Address is not Valid";
+                        lblMsg.ForeColor = Color.Red;
+                    }
+                }
+                else
+                {
+                    lblMsg.Text = "IP Address is not Valid";
+                    lblMsg.ForeColor = Color.Red;
+                }
+            }
+
+            if (int.Parse(txt1.Text) >= 192 && int.Parse(txt1.Text) <= 223)
+            {
+                if (flag7 == -1)
+                {
+                    if (t1 == true && t2 == true && t3 == true && t4 == true)
+                    {
+
+                        lblMsg.Text = "IP Address is Valid";
+                        lblMsg.ForeColor = Color.Green;
+                    }
+                    else
+                    {
+                        lblMsg.Text = "IP Address is not Valid";
+                        lblMsg.ForeColor = Color.Red;
+                    }
+                }
+                else
+                {
+                    lblMsg.Text = "IP Address is not Valid";
+                    lblMsg.ForeColor = Color.Red;
+                }
+            }
+
+
+        }
+
+        private void BtnCalc_Click(object sender, EventArgs e)
+        {
+            if (int.Parse(txt1.Text) >= 0 && int.Parse(txt1.Text) <= 127)
+            {
+               m1.Text="255";
+               m2.Text = "0";
+               m3.Text = "0";
+               m4.Text = "0";
+            }
+            else if (int.Parse(txt1.Text) >= 128 && int.Parse(txt1.Text) <= 191)
+            {
+                m1.Text = "255";
+                m2.Text = "255";
+                m3.Text = "0";
+                m4.Text = "0";
+            }
+            else if (int.Parse(txt1.Text) >= 192 && int.Parse(txt1.Text) <= 223)
+            {
+                m1.Text = "255";
+                m2.Text = "255";
+                m3.Text = "255";
+                m4.Text = "0";
+            }
+            else 
+            {
+                m1.Text = "";
+                m2.Text = "";
+                m3.Text = "";
+                m4.Text = "";
+            }
+
+        }
+        
+            
+
+    private void button5_Click(object sender, EventArgs e)
+        {
+            {
+               
+
+               
+            }
+            {
+               label5.Text = convert_to_Binary(int.Parse(txt1.Text));
+                label6.Text = convert_to_Binary(int.Parse(txt2.Text));
+                label7.Text = convert_to_Binary(int.Parse(txt3.Text));
+                label8.Text = convert_to_Binary(int.Parse(txt4.Text));
+            }
+
+
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            if(flag1==1)
+            {
+                if(flag2==1)
+                {
+                    lblMsg.Text = txt1.Text + ".255.255.255";
+                }
+                if (flag2 == 2)
+                {
+                    lblMsg.Text = txt1.Text +"."+txt2.Text+ ".255.255";
+                }
+                if (flag2 == 3)
+                {
+                    lblMsg.Text = txt1.Text + "." + txt2.Text + "."+txt3.Text+".255";
+                }
+                if (flag2 == 4)
+                {
+                    MessageBox.Show("Class D and E has no broadacast");
+                }
+            }
+
+
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            if (flag1 == 1)
+            {
+                if (flag2 == 1)
+                {
+                    lblMsg.Text = txt1.Text + ".0.0.0";
+                }
+                if (flag2 == 2)
+                {
+                    lblMsg.Text = txt1.Text + "." + txt2.Text + ".0.0";
+                }
+                if (flag2 == 3)
+                {
+                    lblMsg.Text = txt1.Text + "." + txt2.Text + "." + txt3.Text + ".0";
+                }
+                if (flag2 == 4)
+                {
+                    MessageBox.Show("Class D and E has no broadacast");
+                }
+            }
+        }
+    }
+    }
+
